72d60f372a2c8c4171b901585da8d714fa72518e
Piet Althoff <pietalthoff@googlemail.com>
2019-11-12 16:10:18 +0100

    faultline multithreaded

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-# faultline
-Generate terrain images using fault line formation algorithm
 
-10 iterations             |  50 iterations | 100 iterations
-:-------------------------:|:-------------------------:|:---------------------------:
-![](https://github.com/barisusakli/faultline/blob/master/10_iterations.png)  |  ![](https://github.com/barisusakli/faultline/blob/master/50_iterations.png) | ![](https://github.com/barisusakli/faultline/blob/master/100_iterations.png)
+# faultline (multithreaded)
 
-500 iterations             |  1000 iterations | 2000 iterations
+Generate terrain images using fault line formation algorithm.
+
+This version uses node's [worker_threads](https://nodejs.org/api/worker_threads.html) to calculate the image generation function on multiple cores. It just adds a couple of lines code and is still fairly easy to read.
+
+On an AMD-FX-6100 it's twice as fast as the single-threaded algorithm.
+1024x1024px & 2000 iterations :
+
+ - single-threaded: 13849 ms 
+ - multithreaded: 6723 ms
+
+The Color-Buffer-Generation is still expensive (~600 ms) and could be optimized in a similar way.
+
+ Forked from [faultline](https://github.com/barisusakli/faultline) by [barisusakli](https://github.com/barisusakli)
+
+10 iterations | 50 iterations | 100 iterations
+
 :-------------------------:|:-------------------------:|:---------------------------:
-![](https://github.com/barisusakli/faultline/blob/master/500_iterations.png)  |  ![](https://github.com/barisusakli/faultline/blob/master/1000_iterations.png) | ![](https://github.com/barisusakli/faultline/blob/master/2000_iterations.png)
+
+![](https://github.com/barisusakli/faultline/blob/master/10_iterations.png) | ![](https://github.com/barisusakli/faultline/blob/master/50_iterations.png) | ![](https://github.com/barisusakli/faultline/blob/master/100_iterations.png)
+
+  
+
+500 iterations | 1000 iterations | 2000 iterations
+
+:-------------------------:|:-------------------------:|:---------------------------:
+
+![](https://github.com/barisusakli/faultline/blob/master/500_iterations.png) | ![](https://github.com/barisusakli/faultline/blob/master/1000_iterations.png) | ![](https://github.com/barisusakli/faultline/blob/master/2000_iterations.png)
--- index.js
+++ index.js
@@ -1,144 +1,214 @@
 'use strict';
 
-const Jimp = require('jimp');
-const util = require('util');
+const {
+    Worker, isMainThread, parentPort, workerData
+} = require('worker_threads');
 
-const width = 256;
-const height = 256;
-const numIterations = 2000;
-let maxHeight = 0;
-let minHeight = 0;
+if (isMainThread) {
+    /*
+    ███╗   ███╗ █████╗ ██╗███╗   ██╗
+    ████╗ ████║██╔══██╗██║████╗  ██║
+    ██╔████╔██║███████║██║██╔██╗ ██║
+    ██║╚██╔╝██║██╔══██║██║██║╚██╗██║
+    ██║ ╚═╝ ██║██║  ██║██║██║ ╚████║
+    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝
 
-const colorRamp = [
-	[0,0,100],
-	[0,0,100],
-	[0,0,100],
-	[0,0,100],
-	[0,0,100],
-	[0,0,100],
-	[0,0,155],
-	[0,0,170],
-	[0,0,180],
-	[0,0,255],
-	[0,0,255],
-	[0,0,255],
-	[0,0,255],
-	[0,127,127],
-	[0,255,0],
-	[42,255,0],
-	[85,255,0],
-	[127,255,0],
-	[170,255,0],
-	[212,255,0],
-	[255,255,0],
-	[246,226,0],
-	[238,197,0],
-	[230,168,0],
-	[222,139,0],
-	[214,110,0],
-	[206,81,0],
-	[214,110,42],
-	[222,139,85],
-	[230,168,127],
-	[238,197,170],
-	[246,226,212],
-	[255,255,255],
-];
+    */
+    const Jimp = require('jimp');
+    const util = require('util');
+    
+    const os = require('os');
+    const threadCount = os.cpus().length;
+    
+    // consts
+    const width = 1024;
+    const height = 1024;
+    const numIterations = 2000;
+    let maxHeight = 0;
+    let minHeight = 0;
 
-async function generateImage() {
-	const heightData = new Array(width * height).fill(0);
+    const colorRamp = [
+        [0, 0, 100],
+        [0, 0, 100],
+        [0, 0, 100],
+        [0, 0, 100],
+        [0, 0, 100],
+        [0, 0, 100],
+        [0, 0, 155],
+        [0, 0, 170],
+        [0, 0, 180],
+        [0, 0, 255],
+        [0, 0, 255],
+        [0, 0, 255],
+        [0, 0, 255],
+        [0, 127, 127],
+        [0, 255, 0],
+        [42, 255, 0],
+        [85, 255, 0],
+        [127, 255, 0],
+        [170, 255, 0],
+        [212, 255, 0],
+        [255, 255, 0],
+        [246, 226, 0],
+        [238, 197, 0],
+        [230, 168, 0],
+        [222, 139, 0],
+        [214, 110, 0],
+        [206, 81, 0],
+        [214, 110, 42],
+        [222, 139, 85],
+        [230, 168, 127],
+        [238, 197, 170],
+        [246, 226, 212],
+        [255, 255, 255],
+    ];
 
-	for (let i = 0; i < numIterations; i++) {
-		shiftSide(heightData);
-		// if ([10, 50, 100, 500, 1000, 2000].includes(i + 1)) {
-		// 	findMaxMinHeight(heightData);
-		// 	await saveImage(heightDataToColorBuffer(heightData), (i + 1) + '_iterations.png');
-		// }
-	}
-	findMaxMinHeight(heightData);
+    // functions
+    // generate random line by picking 2 random points
+    const generateRandomLines = (amount) => {
+        let lines = [];
+        for (let i = 0; i < amount; i++) {
+            const x1 = getRandomInt(0, width - 1);
+            const y1 = getRandomInt(0, height - 1);
+            const x2 = getRandomInt(0, width - 1);
+            const y2 = getRandomInt(0, height - 1);
+            lines.push([x1,y1,x2,y2]);
+        }
+        return lines;
+    }
 
-	return heightDataToColorBuffer(heightData);
+    // min, max inclusive
+    const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const findMaxMinHeight = (heightData) => {
+        heightData.forEach((h) => {
+            if (h > maxHeight) {
+                maxHeight = h;
+            }
+            if (h < minHeight) {
+                minHeight = h;
+            }
+        });
+    }
+
+    const normalize = (num, min, max) => {
+        return (num - min) / (max - min);
+    }
+
+    //still fairly expensive on large images
+    const heightDataToColorBuffer = (heightData) => {
+        let colorData = [];
+        for (let i = 0; i < height; i++) {
+            for (let k = 0; k < width; k++) {
+                let currentHeight = heightData[k + i * width];
+                let colorIndex = Math.floor(normalize(currentHeight, minHeight, maxHeight) * (colorRamp.length - 1));
+                colorData.push(...colorRamp[colorIndex]);
+            }
+        }
+        return Buffer.from(colorData);
+    }
+
+    // save buffer to file, buffer is rgb data
+    const saveImage = util.promisify(function (buffer, filename, callback) {
+        new Jimp({ data: buffer, width: width, height: height }, (err, image) => {
+            if (err) {
+                return callback(err);
+            }
+            image.write(filename, callback)
+        });
+    });
+
+    /*
+     ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ████████╗███████╗    ██╗███╗   ███╗ █████╗  ██████╗ ███████╗
+    ██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔════╝    ██║████╗ ████║██╔══██╗██╔════╝ ██╔════╝
+    ██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║   ██║   █████╗      ██║██╔████╔██║███████║██║  ███╗█████╗
+    ██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║   ██║   ██╔══╝      ██║██║╚██╔╝██║██╔══██║██║   ██║██╔══╝
+    ╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║   ██║   ███████╗    ██║██║ ╚═╝ ██║██║  ██║╚██████╔╝███████╗
+     ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+
+    */
+
+    const generateImage = async () => {
+    // generate randomlines
+    const randomLines = generateRandomLines(numIterations);
+    // seperate y-axis for threads
+    let chunkSize = (Math.ceil(height / threadCount));
+  
+    /// spin up workers and send worker data (lines and offsets)
+    let workers = [];
+    for (let thread = 0; thread < threadCount; thread++) {
+        let yOffset = [thread * chunkSize, (thread < (threadCount - 1) ? ((thread + 1) * chunkSize) - 1 : height)];
+        workers.push(
+            new Promise((resolve, reject) => {
+                const worker = new Worker(__filename, {
+                    workerData: { randomLines, yOffset, width}
+                });
+                worker.on('message', resolve);
+                worker.on('error', reject);
+                worker.on('exit', (code) => {
+                    if (code !== 0)
+                        reject(new Error(`Worker stopped with exit code ${code}`));
+                });
+            })
+        );
+    };
+    let heightData = await Promise.all(workers);
+    heightData = heightData.flat();
+    findMaxMinHeight(heightData);
+    return heightDataToColorBuffer(heightData);
+    };
+
+
+    const start = async () => {
+        try {
+            const now = Date.now();
+            const buffer = await generateImage();
+            await saveImage(buffer, 'test.png');
+            console.log(numIterations + ' iterations took ' + (Date.now() - now) + ' ms');
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    start();
+
+
+} else {
+
+
+    /*
+    ██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗███████╗██████╗
+    ██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝██╔════╝██╔══██╗
+    ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ █████╗  ██████╔╝
+    ██║███╗██║██║   ██║██╔══██╗██╔═██╗ ██╔══╝  ██╔══██╗
+    ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗███████╗██║  ██║
+     ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+
+    */
+    // https://math.stackexchange.com/a/274728/49923
+    const pointSide = (x, y, x1, y1, x2, y2) => {
+        return ((x - x1) * (y2 - y1)) - ((y - y1) * (x2 - x1));
+    }
+
+    const { randomLines, yOffset, width} = workerData;
+    const heightData = new Array(((yOffset[1] - yOffset[0])+1) * width).fill(0);
+    for (let line of randomLines) {
+        let stride = 0;
+        for (let y = yOffset[0]; y <= yOffset[1]; y++) {
+            for (let x = 0; x < width; x++) {
+                const d = pointSide(x, y, line[0], line[1], line[2], line[3]);
+                const i = x + (stride * width);
+                if (d > 0) {
+                    heightData[i]++;
+                } else if (d < 0) {
+                    heightData[i]--;
+                }
+            }
+            stride++;
+        }
+    }
+    parentPort.postMessage(heightData);
 }
-
-function findMaxMinHeight(heightData) {
-	heightData.forEach((h) => {
-		if (h > maxHeight) {
-			maxHeight = h;
-		}
-		if (h < minHeight) {
-			minHeight = h;
-		}
-	});
-}
-
-function heightDataToColorBuffer(heightData) {
-	const colorData = [];
-	for (let i = 0; i < height; i++) {
-		for (let k = 0; k < width; k++) {
-			let currentHeight = heightData[k + i * width];
-			let colorIndex = Math.floor(normalize(currentHeight, minHeight, maxHeight) * (colorRamp.length - 1));
-			colorData.push(...colorRamp[colorIndex]);
-		}
-	}
-
-	return Buffer.from(colorData);
-}
-
-function normalize(num, min, max) {
-	return (num - min) / (max - min);
-}
-
-// generate random line by picking 2 random points
-// go through all points and raise/lower them depending
-// on which side of the line segment they lie on
-function shiftSide(heightData) {
-	const x1 = getRandomInt(0, width - 1);
-	const y1 = getRandomInt(0, height - 1);
-	const x2 = getRandomInt(0, width - 1);
-	const y2 = getRandomInt(0, height - 1);
-	for (let y = 0; y < height; y++) {
-		const stride = y * width;
-		for (let x = 0; x < width; x++) {
-			const d = pointSide(x, y, x1, y1, x2, y2);
-			const i = x + stride;
-			if (d > 0) {
-				heightData[i] ++;
-			} else if (d < 0) {
-				heightData[i] --;
-			}
-		}
-	}
-}
-
-// min, max inclusive
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// https://math.stackexchange.com/a/274728/49923
-function pointSide(x, y, x1, y1, x2, y2) {
-	return ((x - x1) * (y2 - y1)) - ((y - y1) * (x2 - x1));
-}
-
-// save buffer to file, buffer is rgb data
-const saveImage = util.promisify(function (buffer, filename, callback) {
-	new Jimp({ data: buffer, width: width, height: height }, (err, image) => {
-		if (err) {
-			return callback(err);
-		}
-		image.write(filename, callback)
-	});
-});
-
-(async () => {
-	try {
-		const now = Date.now();
-		const buffer = await generateImage();
-		await saveImage(buffer, 'test.png');
-		console.log(numIterations + ' iterations took ' + (Date.now() - now) + ' ms');
-	} catch (e) {
-		console.error(e);
-	}
-})();
